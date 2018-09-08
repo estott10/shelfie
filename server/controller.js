09e61c490756a3b0f45932efb2467b424a47e155
ErikaStott <erika.r.stott@gmail.com>
2018-09-08 08:38:13 -0700

    through post

--- server/controller.js
+++ server/controller.js
@@ -1,50 +1,49 @@
 module.exports = {
-          
+
     getInventory: (req, res, next) => {
-        const dbInstance= req.app.get('db');
+        const dbInstance = req.app.get('db');
 
         dbInstance.get_inventory()
-            .then( result => {
-                res.send(result) 
+            .then(result => {
+                res.send(result)
             })
-            .catch( err => {
-                res.status(500).send({errorMessage: "failed to retreive product"});
+            .catch(err => {
+                res.status(500).send({ errorMessage: "failed to retreive product" });
                 console.log(err)
-            }) 
+            })
     },
 
     addInventory: (req, res, next) => {
-        const dbInstance= req.app.get('db');
-        const { product_name, price, image} = req.body
+        const dbInstance = req.app.get('db');
+        const { product_name, price, image } = req.body
 
         dbInstance.create_product([product_name, price, image])
-            .then( result => {
-                
+            .then(result => {
+
                 dbInstance.get_inventory()
-                .then( result => {
-                res.send(dbInstance.get_inventory()
-                .then( result => {
-                    res.send(result)
-                }))
+
+                    .then(result => {
+                        res.send(result)
                     })
             })
-            .catch( err => {
-                res.status(500).send({errorMessage: "failed to create product"});
+
+            .catch(err => {
+                res.status(500).send({ errorMessage: "failed to create product" });
                 console.log(err)
-            }) 
+            })
     },
 
-       deleteInventory: (req, res, next) => {
-           const dbInstance= req.app.get('db');
-         
-           dbInstance.delete_inventory(req.params.id)
-             .then(result => {
+    deleteInventory: (req, res, next) => {
+        const dbInstance = req.app.get('db');
+
+        dbInstance.delete_inventory(req.params.id)
+            .then(result => {
                 res.sendStatus(200)
-                })
-                .catch( err => {
-                    res.status(500).send({errorMessage: "failed to retreive product"});
-                    console.log(err)
-            }) 
-        }
+            })
+            .catch(err => {
+                res.status(500).send({ errorMessage: "failed to retreive product" });
+                console.log(err)
+            })
+    }
 
 }
